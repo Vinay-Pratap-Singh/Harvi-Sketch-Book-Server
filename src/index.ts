83fd940c53ad865a6ec1e9587cbf30f625b9d5a6
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-12-10 00:00:52 +0530

    feat: setup socket server

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,27 @@
-// src/index.ts
-import express from "express";
+require("dotenv").config();
+import { Socket } from "socket.io";
+
+const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
 
 const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello, Express with TypeScript!");
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    optionsSuccessStatus: 200,
+  })
+);
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  },
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+io.on("connection", (socket: Socket) => {
+  console.log("socket connected ...");
 });
+
+httpServer.listen(process.env.PORT || 5000);

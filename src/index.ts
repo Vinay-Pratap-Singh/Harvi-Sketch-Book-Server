fe15cd5c2378421459211ac1eb01590cd1d4658f
Vinay Pratap Singh <technicalharvi@gmail.com>
2023-12-24 09:38:23 +0530

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,12 @@ import {
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    origin: (process.env.FRONTEND_URL as string) || "http://localhost:3000",
+    credentials: true,
     optionsSuccessStatus: 200,
   })
 );
